b1ac46dacc6aa59879fc20062865f2a694f9df6d
abnrk <adudowas@gmail.com>
2025-06-02 20:53:10 +0100

    Update user.js

--- user.js
+++ user.js
@@ -1,6 +1,5 @@
 // use with extension: https://addons.mozilla.org/en-US/firefox/addon/maximize-windows-on-start-up/
 user_pref("privacy.history.custom",true);
 user_pref("privacy.resistFingerprinting.letterboxing",false);
-user_pref("privacy.history.custom",true);
 user_pref("privacy.resistFingerprinting.letterboxing.rememberSize",true);
 user_pref("privacy.resistFingerprinting.resizeWarnings",0);
